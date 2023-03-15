a96e1288f5d25e1f28b52d7ed1c536c19ae64790
Dawid Karp <dadulerk@gmail.com>
2023-03-15 16:44:55 +0100

    add bisection algorithm in rust

--- rust/root-finding/src/main.rs
+++ rust/root-finding/src/main.rs
@@ -19,6 +19,30 @@
 //     inner_loop(initial_guess, g)
 // }
 
+fn bisect(f: fn(f64) -> f64, min: f64, max: f64) -> Option<f64> {
+    let mut a = min;
+    let mut b = max;
+
+    let mut i = 0;
+
+    loop {
+        let midpoint = a + (b - a) / 2.0;
+
+        println!("{i}: {midpoint}");
+        i += 1;
+
+        if f64::abs(f(midpoint)) < 1e-4 {
+            return Some(midpoint);
+        } else if f(midpoint) * f(a) > 0.0 {
+            a = midpoint;
+            continue;
+        } else {
+            b = midpoint;
+            continue;
+        }
+    }
+}
+
 fn stable_point(g: fn(f64) -> f64, initial_guess: f64) -> Option<f64> {
     let mut x = initial_guess;
 
@@ -43,6 +67,10 @@ fn stable_point(g: fn(f64) -> f64, initial_guess: f64) -> Option<f64> {
 
 fn main() {
     // f(x) = x**2 - 6x + 8 = 0
+    let f = |x: f64| x * x - 6.0 * x + 8.0;
+    println!("{:?}", bisect(f, -2.0, 9.0));
+
+    println!();
 
     // x = sqrt(6x - 8)
     let g = |x| f64::sqrt(6.0 * x - 8.0);
